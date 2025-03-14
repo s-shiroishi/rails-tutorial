635c7ad72397997d6e08b6c43ed080749fc00ad1
estyle-shiroishi <s.shiroishi@estyle.inc.jp>
2025-03-14 21:47:24 +0900

    Fin Chap3

--- core/config/routes.rb
+++ core/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/', to: 'static_pages#home'
+  root to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
 end
--- core/spec/requests/static_pages_spec.rb
+++ core/spec/requests/static_pages_spec.rb
@@ -1,24 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
-  describe "GET /home" do
-    it "成功" do
-      get "/home"
-      expect(response).to have_http_status(:success)
+  describe "GETリクエスト" do
+    context '/' do
+      it "成功" do
+        get root_path
+        expect(response).to have_http_status(:success)
+      end
     end
-  end
 
-  describe "GET /help" do
-    it "成功" do
-      get "/help"
-      expect(response).to have_http_status(:success)
+    context "/help" do
+      it "成功" do
+        get help_path
+        expect(response).to have_http_status(:success)
+      end
     end
-  end
 
-  describe "GET /about" do
-    it "成功" do
-      get "/about"
-      expect(response).to have_http_status(:success)
+    context "/about" do
+      it "成功" do
+        get about_path
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 
