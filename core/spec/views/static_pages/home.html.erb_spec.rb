25576be3c90e0198b763c8e1f19d63b43b43cca2
estyle-shiroishi <s.shiroishi@estyle.inc.jp>
2025-03-15 11:22:27 +0900

    Fin Chap4

--- core/spec/views/static_pages/home.html.erb_spec.rb
+++ core/spec/views/static_pages/home.html.erb_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe "static_pages/home.html.erb", type: :view do
     it '適切なタイトルが含まれている' do
         render template: 'static_pages/home', layout: "layouts/application"
-        expect(rendered).to have_title('Home | Ruby on Rails Tutorial Sample App')
+        expect(rendered).to have_title('Ruby on Rails Tutorial Sample App')
     end
 end
